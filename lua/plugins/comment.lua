91f63726ecf085fb20e2d89b910681f0a9890227
Charlestran267 <charlestran267@gmail.com>
2024-04-19 20:43:41 +0800

    add tab bar

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,2 @@
-return {
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-}
+return -- Highlight todo, notes, etc in comments
+{ 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
