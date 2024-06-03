6784e0cf42074fc24578e2d72800b31cf34adfb0
CharlesTran267 <charlestran267@gmail.com>
2024-06-03 07:02:02 +0700

    add old_git_status

--- lua/plugins/oil_nvim.lua
+++ lua/plugins/oil_nvim.lua
@@ -1,10 +1,30 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('oil').setup()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-  end,
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+        },
+        win_options = {
+          signcolumn = 'yes:2',
+        },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
+  {
+    'refractalize/oil-git-status.nvim',
+    dependencies = {
+      'stevearc/oil.nvim',
+    },
+    config = function()
+      require('oil-git-status').setup {
+        show_ignored = true,
+      }
+    end,
+  },
 }
