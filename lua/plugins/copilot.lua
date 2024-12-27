df400ef45abb8f1ebd84a8d034943c9a4fcebaf5
Charles Tran <charlestran267@gmail.com>
2024-12-27 12:14:08 +0800

    use main branch copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,6 @@ return {
   },
   {
     'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
     dependencies = {
       { 'github/copilot.vim' },
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
