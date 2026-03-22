d0587094c7dcf50ab5beaeda1ec34da7509351d3
Charles Tran <charlestran267@gmail.com>
2026-03-22 16:40:16 +0800

    refactor: improve toggleterm configuration with lazy terminal creation and dynamic selection

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,10 +2,10 @@ return {
   'akinsho/toggleterm.nvim',
   branch = 'main',
   config = function()
-    local toggleterm = require('toggleterm')
+    local toggleterm = require 'toggleterm'
     local Terminal = require('toggleterm.terminal').Terminal
 
-    toggleterm.setup({
+    toggleterm.setup {
       direction = 'float',
       highlights = {
         FloatBorder = {
@@ -21,45 +21,60 @@ return {
           return ' Terminal ' .. term.id
         end,
       },
-    })
+    }
 
-    -- Create predefined terminals
-    local random_term = Terminal:new({
-      display_name = 'random',
-      direction = 'float',
-      count = 1,
-    })
+    -- Predefined terminal configs (lazy - only created when first accessed)
+    local terminal_configs = {
+      lazygit = { cmd = 'lazygit', display_name = 'lazygit', direction = 'float' },
+      aider = { cmd = 'aider', display_name = 'aider', direction = 'float' },
+      codex = { cmd = 'codex', display_name = 'codex', direction = 'float' },
+      aichat = { cmd = 'aichat', display_name = 'aichat', direction = 'float' },
+    }
 
-    local lazygit_term = Terminal:new({
-      cmd = 'lazygit',
-      display_name = 'lazygit',
-      direction = 'float',
-      count = 2,
-    })
+    local terminals = {}
+    local last_terminal = nil
 
-    local aider_term = Terminal:new({
-      cmd = 'aider',
-      display_name = 'aider',
-      direction = 'float',
-      count = 3,
-    })
+    local function get_terminal(name)
+      if not terminals[name] then
+        terminals[name] = Terminal:new(terminal_configs[name])
+      end
+      return terminals[name]
+    end
 
-    local codex_term = Terminal:new({
-      cmd = 'codex',
-      display_name = 'codex',
-      direction = 'float',
-      count = 4,
-    })
+    local function toggle_terminal(name)
+      local term = get_terminal(name)
+      last_terminal = name
+      term:toggle()
+    end
 
-    -- Keymaps for each terminal
-    vim.keymap.set({ 'n', 't' }, '<A-i>', function() random_term:toggle() end, { desc = 'Toggle random terminal' })
-    vim.keymap.set({ 'n', 't' }, '<A-g>', function() lazygit_term:toggle() end, { desc = 'Toggle lazygit' })
-    vim.keymap.set({ 'n', 't' }, '<A-a>', function() aider_term:toggle() end, { desc = 'Toggle aider' })
-    vim.keymap.set({ 'n', 't' }, '<A-c>', function() codex_term:toggle() end, { desc = 'Toggle codex' })
+    -- Toggle last-used terminal (defaults to a general terminal)
+    vim.keymap.set({ 'n', 't' }, '<A-i>', function()
+      if last_terminal then
+        toggle_terminal(last_terminal)
+      else
+        vim.cmd 'ToggleTerm direction=float'
+      end
+    end, { desc = 'Toggle last terminal' })
 
-    -- Quick select terminal
-    vim.keymap.set({ 'n', 't' }, '<A-s>', '<Cmd>TermSelect<CR>', { desc = 'Select terminal' })
+    -- Terminal picker
+    vim.keymap.set({ 'n', 't' }, '<A-s>', function()
+      local options = { 'lazygit', 'aider', 'codex', 'aichat', 'terminal', 'new terminal' }
+      vim.ui.select(options, { prompt = 'Select terminal:' }, function(choice)
+        if not choice then
+          return
+        end
+        if choice == 'terminal' then
+          vim.cmd 'ToggleTerm direction=float'
+        else
+          toggle_terminal(choice)
+        end
+      end)
+    end, { desc = 'Select terminal' })
+
+    -- Close current terminal
+    vim.keymap.set('t', '<A-q>', '<Cmd>close<CR>', { desc = 'Close terminal' })
+
     -- Create new terminal
-    vim.keymap.set({ 'n', 't' }, '<A-n>', '<Cmd>TermNew direction=float<CR>', { desc = 'New terminal' })
+    vim.keymap.set({ 'n', 't' }, '<A-n>', '<Cmd>ToggleTerm direction=float<CR>', { desc = 'New terminal' })
   end,
 }
